78b34b3c6f0d44bb876fa86b2e6977e9d3ac6bbd
Jerry Chen <jchen1766@gmail.com>
2022-02-05 01:08:45 -0500

    Add files via upload

--- HW1/python_main.py
+++ HW1/python_main.py
@@ -3,21 +3,22 @@ import sys
 import cv2
 from PIL import Image
 
-def normalize(x,width,height,inverse):
+def normalize(width,height):
     normMat = np.array([[width+height,0,width/2],[0,width+height,height/2],[0,0,1]])
     # normMat = np.linalg.inv(normMat)
-    # print(normMat)
-    if (inverse):
-        result = np.dot(np.linalg.inv(normMat), x)
-    else:
-        result = np.matmul(normMat, x)
-    return result
+    Tn = np.linalg.inv(normMat)
+    # print(Tn)
+    # if (inverse):
+    #     result = np.matmul(normMat, x)
+    # else:
+    #     result = np.matmul(Tn, x)
+    return Tn
 
 
 def computeA(source, target):
     [a,b,c] = source
     [x,y,z] = target
-    matrixA = np.array([[0,0,0,-a,-b,-c,y*a,y*b,y*c],[a,b,c,0,0,0,-x*a,-x*b,-x*c]])
+    matrixA = np.array([[0,0,0,-x,-y,-z,y*a,y*b,y*c],[a,b,c,0,0,0,-x*a,-x*b,-x*c]])
     return matrixA
 
 def imageWarp(originalImg, newImg, H):
@@ -25,16 +26,16 @@ def imageWarp(originalImg, newImg, H):
     new = np.array(newImg)
     (row, col) = originalImg.size
     (w,h) = newImg.size
-    for i in range(0,h):
-        for j in range(0,w):
-            coord = [j,i,1]
+    for i in range(0,w):
+        for j in range(0,h):
+            coord = [i,j,1]
             res = np.matmul(H, coord)
             # print(res)
             x = res[0]/res[2]
             y = res[1]/res[2]
             oxt = str(x).split('.')
             oyt = str(y).split('.')
-            # print(x,y)
+            print(x,y)
             # new[j][i] = original[round(x,0)][round(y,0)]
             if(len(oxt[1]) <= 1 or len(oyt[1]) <= 1):
                 new[i][j] = original[int(y)][int(x)]
@@ -69,12 +70,12 @@ def main():
     # normalize arr2 and arr2
     for i in range(0,4):
         # print(arr1[i])
-        arr1[i] = normalize(arr1[i],488,366,False)
-        arr2[i] = normalize(arr2[i],940,500,False)
-    print(arr1)
-    print(arr2)
+        arr1[i] = np.matmul(normalize(488,366),arr1[i])
+        arr2[i] = np.matmul(normalize(940,500),arr2[i])
+    # print(arr1)
+    # print(arr2)
     # compute A matrix
-    A = np.zeros((8,9),dtype=int)
+    A = np.zeros((8,9))
     for i in range(0,4):
         Ai = computeA(arr1[i],arr2[i])
         # print(Ai)
@@ -85,16 +86,22 @@ def main():
 
     U, D, VT = np.linalg.svd(A)
     H = VT[8]
-    
     H = np.reshape(H,(3,3))
-    # print(H)
+    print(H)
     H = np.linalg.inv(H)
-    # print(H)
+    print(H)
+
+    # Denormalize H 
+    # H = np.matmul(np.linalg.inv(normalize(488,366)),np.matmul(H,normalize(940,500)))
+    H = np.matmul(np.matmul(np.linalg.inv(normalize(940,500)),H),normalize(488,366))
+    print(H)
+
+    
     # H = normalize(H,940,500,False)
     # H = normalize(H,488,366,True)
     # print(H)
-    # result = imageWarp(image,newImg,H)
-    # result.show()
+    result = imageWarp(image,newImg,H)
+    result.show()
 
 if __name__ == "__main__":
     main()
